c1fd65a424d11e74075e95b4cbd914d4f7b1cf8a
Hakeem0598 <ayo75@icloud.com>
2023-10-02 21:12:33 +0100

    Update subdomain

--- terraform/global/main.tf
+++ terraform/global/main.tf
@@ -8,7 +8,7 @@ resource "aws_acm_certificate" "cert" {
 
   domain_name               = var.domain_name
   validation_method         = "DNS"
-  subject_alternative_names = ["*.${var.domain_name}", "*.staging.${var.domain_name}"]
+  subject_alternative_names = ["*.${var.domain_name}"]
 
   lifecycle {
     create_before_destroy = true
