ae4af6a75db08febba4e453f178d0d9a24cb1bc9
Hakeem0598 <ayo75@icloud.com>
2023-10-02 21:18:26 +0100

    Update oac name

--- terraform/modules/cdn/main.tf
+++ terraform/modules/cdn/main.tf
@@ -3,7 +3,7 @@ locals {
 }
 
 resource "aws_cloudfront_origin_access_control" "oac" {
-  name                              = "${var.web_bucket_regional_domain_name}-${var.environment}"
+  name                              = "oac-${var.environment}"
   origin_access_control_origin_type = "s3"
   signing_behavior                  = "always"
   signing_protocol                  = "sigv4"
