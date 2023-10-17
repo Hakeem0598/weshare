29e021c07669c2c342d0cd65a304fc07d180e28d
Hakeem0598 <ayo75@icloud.com>
2023-10-17 15:42:02 +0100

    Remove redundant files

--- packages/backend/src/types.ts
+++
@@ -1,9 +0,0 @@
-export enum AWSRegions {
-	'eu-west-2' = 'eu-west-2',
-}
-
-export const DEFAULT_EXPIRY = 24 * 60 * 60;
-
-export const BUCKET_NAME = process.env.BUCKET_NAME!;
-
-export const BASE_URL = process.env.BASE_URL!;
