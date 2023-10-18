02b4ff092a6e853af3f2d837ccfade7ee72a83fa
Hakeem0598 <ayo75@icloud.com>
2023-10-18 22:23:10 +0100

    Add sign in logic

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,11 +1,40 @@
+import { useEffect } from 'react';
+import { request } from './api';
 import Button from './components/Button/Button';
 import CallToAction from './components/CallToAction/CallToAction';
 import Friends from './components/Friends/Friends';
 import { Header } from './components/Header/Header';
 import Sidebar from './components/Sidebar/Sidebar';
 import UploadContainer from './components/UploadContainer/UploadContainer';
+import { parseQueryString } from './utils/parseQueryString';
 
 function App() {
+	useEffect(() => {
+		(async () => {
+			const url = new URL(window.location.href);
+
+			if (url.searchParams.size !== 2) return;
+
+			const queryString = url.search.substring(1);
+
+			const queryObj = parseQueryString(queryString);
+
+			if (!queryObj['code'] || !queryObj['state']) return;
+
+			const res = await request(`/auth/callback${url.search}`);
+
+			if (res.status !== 200) return;
+
+			const { access_token } = res.data;
+
+			request.defaults.headers.common = {
+				Authorization: `Bearer ${access_token}`,
+			};
+
+			window.location.replace(window.location.origin);
+		})();
+	}, []);
+
 	return (
 		<div className='p-[--app-padding] h-full relative'>
 			<Header />
